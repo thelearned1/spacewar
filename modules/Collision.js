fb85c110d3e7ddf64586bfcc09d4cbd38296ac6c
Ned Watson <watsonle@iastate.edu>
2022-09-09 13:56:05 -0400

    Add collision detection for convex polygons

--- modules/Collision.js
+++ modules/Collision.js
@@ -9,6 +9,20 @@ class Collider {
 			throw new Error('Cannot instantiate an abstract class')
 		}
 	}
+
+	collides (obj) {
+		if (obj instanceof Point) {
+			return pointNearPoint(this, obj);
+		} else if (obj instanceof Line) {
+			return pointOnLine(this, obj);
+		} else if (obj instanceof Rectangle) {
+			return pointInRect(this, obj) 
+		} else if (obj instanceof Circle) {
+			return pointInCirc(this, obj)
+		} else {
+			return false
+		}
+	}
 }
 
 /**
@@ -21,6 +35,21 @@ class Point extends Collider {
 		this.x=x; 
 		this.y=y;
 	}
+
+	collides (obj) {
+		switch (obj.constructor) {
+			case Point:
+				return pointNearPoint(this, obj);
+			case Line:
+				return pointOnLine(this, obj);
+			case Rectangle:
+				return pointInRect(this, obj) 
+			case Circle:
+				return pointInCirc(this, obj)
+			default:
+				return false
+		}
+	}
 }
 
 /**
@@ -31,7 +60,22 @@ class Line extends Collider {
 		super();
 		this.p1 = new Point(x1,y1);
 		this.p2 = new Point(x2,y2);
-		this.len = euclideanDist(p1, p2);
+		this.len = euclideanDist(this.p1, this.p2);
+	}
+
+	collides (obj) {
+		switch (obj.constructor) {
+			case Point:
+				return pointOnLine(obj, this);
+			case Line:
+				return lineIntersectsLine(this, obj);
+			case Rectangle:
+				return lineIntersectsRect(this, obj);
+			case Circle:
+				return lineInCirc(this, obj);
+			default:
+				return false
+		}
 	}
 }
 
@@ -45,6 +89,21 @@ class Rectangle extends Collider {
 		this.height=h;
 		this.corner = new Point (x,y);
 	}
+
+	collides (obj) {
+		switch (obj.constructor) {
+			case Point: 
+				return pointInRect(obj, this);
+			case Line:
+				return lineIntersectsRect(obj, this)
+			case Rectangle: 
+				return rectIntersectsRect(this, obj)
+			case Circle: 
+				return rectIntersectsCirc(this, obj)
+			default:
+				return false
+		}
+	}
 }
 
 /** Represents a circle in 2D Euclidean space.
@@ -56,6 +115,105 @@ class Circle extends Collider {
 		this.r = r;
 		this.center = new Point(cx,cy);
 	}
+
+	collides (obj) {
+		switch (obj.constructor) {
+			case Point:
+				return pointInCirc(obj, this);
+			case Line:
+				return lineInCirc(obj, this);
+			case Rectangle:
+				return rectIntersectsCirc(obj, this);
+			case Circle:
+				return circIntersectsCirc(obj, this);
+			default: 
+				return false;
+		}
+	}
+}
+
+function dot (vec1, vec2) {
+	if (vec1.length!==vec2.length) {
+		throw new Error('Argument exception: input arrays must be of equal length');
+	} 
+	return vec1.reduce((acc, x, i) => (
+		acc+(x*vec2[i])
+	), 0)
+}
+
+function applySAT (polygon1, polygon2) {
+	let pts1 = polygon1.points, pts2 = polygon2.points, j = 0;
+	for (let j = 1; j < pts1.length; j++) {
+		let i=(j+1 === pts1.length ? 0 : j-1);
+		let len_ax = euclideanDist(pts1[i], pts1[j]);
+		let axis = [-(pts1[j][1]-pts1[i][1])/len_ax, (pts1[j][0]-pts1[i][0])/len_ax];
+		let projections1 = pts1.map(pt => (dot([pt[0], pt[1]], axis))),
+				projections2 = pts2.map(pt => (dot([pt[0], pt[1]], axis)));
+		let max1 = Math.max(...projections1), min1 = Math.min(...projections1),
+				max2 = Math.max(...projections2), min2 = Math.min(...projections2);
+		
+		if (max1 < min2 || max2 < min1) {
+			return false
+		}
+	}
+	return true;
+}
+
+class Polygon extends Collider {
+	constructor (...args) {
+		super()
+		if (args.length<=1) {
+			throw new Error ("ArgumentCountException: polygons must have at least 2 vertices");
+		} 
+		let points = [];
+		args.forEach(x => {
+			if (Array.isArray(x)) {
+				if (x.some(y => (!isFinite(y))||x.length!==2)) {
+					throw new Error ("ArgumentException: all array arguments must be ordered pairs of finite reals");
+				} else {
+					points.push(x);
+				}
+			} else if (x instanceof Point) {
+				points.push([x.x, x.y]);
+			} else {
+				throw new Error ("ArgumentException: all arguments must be either Points or arrays");
+			}
+		})
+		this.points = points;
+	} 
+
+	collides (obj) {
+		let that;
+		switch (obj.constructor) {
+			case Point:
+				that = new Polygon(obj);
+				break
+			case Line:
+				for (let i = 0; i < this.points.length; i++) {
+					let j = (i+1)%this.points.length;
+					let p1 = this.points[i], p2=this.points[j]
+					if (lineIntersectsLine(new Line(p1[0],p1[1],p2[0],p2[1]), obj)) {
+						return true
+					}
+				}
+				return false
+			case Rectangle:
+				let c = obj.corner;
+				let p2 = new Point(c.x, c.y+obj.height), 
+						p3 = new Point(c.x+obj.width, c.y),
+						p4 = new Point(c.x+obj.width, c.y+obj.height);
+				that= new Polygon(c, p2, p3, p4)
+				break
+			case Circle:
+			case Polygon: 
+				that = obj;
+				break
+			default:
+				return false;
+		}
+		return applySAT(this, that)
+	} 
+
 }
 
 /** Takes two points and determines the Euclidean distance between them.
@@ -65,8 +223,13 @@ class Circle extends Collider {
  * @returns a `Number` representing the Euclidean distance between p1 and p2 
  */
 function euclideanDist (p1, p2) {
-	const deltaY = p1.y-p2.y, deltaX=p1.x-p2.x;
-	return (Math.sqrt((deltaY*deltaY)+(deltaX*deltaX)));
+	let deltaY, deltaX;
+	if (p1 instanceof Point) {
+		deltaY = p1.y-p2.y, deltaX=p1.x-p2.x;
+	} else {
+		deltaY = p1[1]-p2[1], deltaX=p1[0]-p2[0];
+	}
+	return (Math.sqrt((deltaY*deltaY)+(deltaX*deltaX)));	
 } 
 
 /** Determines whether two points are within `distance` of each other.
@@ -100,7 +263,7 @@ function pointOnLine (p, l) {
  * @returns 
  */
 function pointInCirc (p, circ) {
-	return euclideanDist(p, circ.center) < circ.r;
+	return euclideanDist(p, circ.center) <= circ.r;
 }
 
 function pointInRect (p, rect) {
@@ -108,19 +271,45 @@ function pointInRect (p, rect) {
 				 (p.y >= rect.corner.y) && (p.y <= rect.corner.y+rect.height);
 }
 
+/**
+ * 
+ */
+function lineIntersectsLine (l1, l2) {
+	let uA = ((l2.p2.x-l2.p1.x)*(l1.p1.y-l2.p1.y) - (l2.p2.y-l2.p1.y)*(l1.p1.x-l2.p1.x)) / 
+					 ((l2.p2.y-l2.p1.y)*(l1.p2.x-l1.p1.x) - (l2.p2.x-l2.p1.x)*(l1.p2.y-l1.p1.y)),
+			uB = ((l1.p2.x-l1.p1.x)*(l1.p1.y-l2.p1.y) - (l1.p2.y-l1.p1.y)*(l1.p1.x-l2.p1.x)) / 
+					 ((l2.p2.y-l2.p1.y)*(l1.p2.x-l1.p1.x) - (l2.p2.x-l2.p1.x)*(l1.p2.y-l1.p1.y));
+
+	 return (uA >= 0 && uA <= 1 && uB >= 0 && uB <= 1)
+}
+
+function lineIntersectsRect (l1, rect) {
+	let p = rect.corner;
+	return lineIntersectsLine(l1, new Line(p.x, p.y, p.x+rect.width, p.y)) ||
+				 lineIntersectsLine(l1, new Line(p.x, p.y, p.x, p.x+rect.height))||
+				 lineIntersectsLine(l1, new Line(p.x, p.y+rect.height, p.x+rect.width, p.y+rect.height))||
+				 lineIntersectsLine(l1, new Line(p.x+rect.width, p.y, p.x+rect.width, p.y+rect.height));
+
+}
 
 function rectIntersectsRect (rect1,rect2) {
 	let c1 = rect1.corner, c2=rect2.corner;
-	return (c1.x+rect1.w >= c2.x) && (c1.x <= c2.x+rect2.w) && 
-				 (c1.y+rect1.h >= c2.y) && (c1.y <= c2.y+rect2.h);
+	return (c1.x+rect1.width >= c2.x)  && (c1.x <= c2.x+rect2.width) && 
+				 (c1.y+rect1.height >= c2.y) && (c1.y <= c2.y+rect2.height);
 } // rectIntersectsRect
 
 function lineInCirc (l, circ) {
 	if (pointInCirc(l.p1, circ) || pointInCirc(l.p2, circ)) {
 		return true;
 	}
-
-
+	let dot = (((circ.center.x-l.p1.x)*(l.p2.x-l.p1.x)) + 
+						 ((circ.center.y-l.p1.y)*(l.p2.y-l.p1.y))) /
+						 (l.len*l.len);
+	let cpt = new Point(l.p1.x+(dot*(l.p2.x-l.p1.x)),
+										  l.p1.y+(dot*(l.p2.y-l.p1.y)));
+	
+	return pointOnLine(cpt, l) && 
+				 (euclideanDist(cpt, circ.center) <= circ.r);
 }
 
 function circIntersectsCirc(circ1, circ2) {
@@ -141,42 +330,6 @@ function rectIntersectsCirc (rect, circ) {
 	return euclideanDist(new Point(closestX, closestY), circ.center) <= circ.r;
 } // rectIntersectsCirc
 
-function _getGoodCollisionAlgorithms (collider) {
-	if (collider instanceof Circle) {
-		return [[pointNearPoint, 2], [pointInCirc, 2], [rectIntersectsCirc, 2], lineInCirc];
-	} else if (collider instanceof Rectangle) {
-		return [[rectIntersectsRect, 1], [rectIntersectsCirc, 1], [pointInRect, 2]];
-	} else if (collider instanceof Line) {
-		return [[pointOnLine, 2], [lineInCirc, 1]];
-	} else if (collider instanceof Point) {
-		return [[pointNearPoint, 1], [pointOnLine, 1], [pointInRect, 1], [pointInCirc, 1]]; 
-	}
-	return false;
-} // _getGoodCollisionAlgorithms
-
-/**
- * 
- * @param {Collider} x 
- * @param {Collider} y 
- * @returns 
- */
-function collides (x, y) {
-	let xColl = _getGoodCollisionAlgorithms(x);
-	let yColl = _getGoodCollisionAlgorithms(y);
-	let coll;
-
-
-	for (let i = 0; i < xColl.length; i++) {
-		for (let j = 0; j < yColl.length; j++) {
-			if (xColl[i][0]===yColl[j][0]) {
-				coll = xColl[i];
-				break;
-			}
-		}
-	}
-	return (coll[0])(...(coll[1]===1 ? [x, y] : [y, x]));
-} // collides
-
 /**
  * Represents a hitbox composed of more fundamental collision geometries. 
  */
@@ -194,27 +347,63 @@ class Hitbox {
 	 * @param Hitbox hbox 
 	 */
 	collidesWith (hbox) {
-		return hbox.elts.any(x => (
-			this.elts.any(y => (
+		return hbox.elts.some(x => (
+			this.elts.some(y => (
 				Collision.collides(x, y)
 			)) 
 		))
 	}
 } // Hitbox
 
-export { Hitbox, Point, Line, Circle, Rectangle, collides, Collider, //}
-				 p1, p2, p3, l1, l2, l3, c1, c2, c3, r1, r2, r3 };
+let polygonSamples = 
+	[new Polygon([0,0],[1,8],[4,4]),
+	new Polygon([3,4],[7,10],[9,0]),
+	new Polygon([8,4],[12,1],[12,5],[10,8])]
 
-let p1 = new Point(0, 0),
-		p2 = new Point(10, 0),
-		p3 = new Point(10, 10),
-		l1 = new Line(0, 0, 10, 0),
-		l2 = new Line(10, 0, 10, 10),
-		l3 = new Line(0, 0, 10, 10),
-		c1 = new Circle(0, 0, 10),
-		c2 = new Circle(11, 11, 1),
-		c3 = new Circle(11, 11, 10),
-		r1 = new Rectangle(0, 0, 10, 10),
-		r2 = new Rectangle(0, 0, 9, 9),
-		r3 = new Rectangle(10, 10, 10, 10);
 
+export { Hitbox, Point, Line, Circle, Rectangle, Polygon, Collider, //}
+				 polygonSamples, applySAT };
+
+// let collisionSamples = 
+// 	[new Point(0, 0), 								//  0
+// 	new Point(10, 0),									//  1
+// 	new Point(10, 10),								//  2
+// 	new Line(0, 0, 10, 0),						//  3
+// 	new Line(10, 0, 10, 10),					//	4
+// 	new Line(0, 0, 10, 10),						// 	5
+// 	new Circle(0, 0, 10),							// 	6
+// 	new Circle(11, 11, 1),						// 	7
+// 	new Circle(11, 11, 10),						// 	8
+// 	new Rectangle(0, 0, 10, 10),			// 	9
+// 	new Rectangle(0, 0, 9, 9),				// 10
+// 	new Rectangle(10, 10, 10, 10)];		// 11
+
+/*
+true	false	false	true	false	true	true	false	false	true	true	false	
+false	true	false	true	true	false	false	false	false	true	false	false	
+false	false	true	false	true	true	false	false	true	true	false	true	
+true	true	false	false	true	true	true	false	false	true	true	false	
+false	true	true	true	false	true	true	false	true	true	false	true	
+true	false	true	true	true	false	true	false	true	true	true	true	
+true	false	false	true	true	true	true	false	true	true	true	false	
+false	false	false	false	false	false	false	true	true	false	false	true	
+false	false	true	false	true	true	true	true	true	true	true	true	
+true	true	true	true	true	true	true	false	true	false	false	false	
+true	false	false	true	false	true	true	false	true	false	false	false	
+false	false	true	false	true	true	false	true	true	false	false	false
+ */
+
+/* 
+true 	false	false	true	false	true	true	false	false	true	true	false 
+false	true	false	true	false	true	true 	false	false	true	false	false	
+false	false	true	false	true	false	false	true	true		
+true	true
+false	false
+true	true
+true	true
+false	false
+false	false
+true	true
+true	false
+false	false
+*/
